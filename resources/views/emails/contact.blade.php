de075774515004019c3aec3166780e582ba8de3c
winner00kambale <vainqueurkambale@gmail.com>
2025-07-01 19:04:08 +0200

    final commit

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -82,7 +82,7 @@
                                         style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; max-width: 100vw; padding: 32px;">
                                         <p
                                             style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; line-height: 1.5em; margin-top: 0; color: #b0adc5; font-size: 12px; text-align: center;">
-                                            © 2025 SOVAC-RDC. All rights reserved.</p>
+                                            © 2025 SOCAV-RDC. All rights reserved.</p>
 
                                     </td>
                                 </tr>
